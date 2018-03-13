4ce0c5f8466cf44743d8c887e91f958421651087
etoledom <etoledom@icloud.com>
2018-03-13 13:54:41 -0300

    Runtime checks code organization. Creating rules based on the `Rule` protocol.

--- WordPress/Classes/Utility/RuntimeChecks.swift
+++ WordPress/Classes/Utility/RuntimeChecks.swift
@@ -1,49 +1,95 @@
 import Foundation
 
+private let rules: [Rule] = [
+    NoAlignLeftRule(),
+    CellsAccessibilityLabelRule()
+]
+
+protocol Rule {
+    var name: String { get }
+    var description: String { get }
+
+    func apply(on view: UIView) -> Bool
+    func printError(for view: UIView, in viewController: UIViewController)
+}
+
+extension Rule {
+    func printError(for view: UIView, in viewController: UIViewController) {
+        let address = unsafeBitCast(view, to: Int.self)
+        DDLogWarn("⚠️ Violation of rule '\(name)': \(description). \n<\(type(of: view)): \(String(format: "%p", address))>, in \(viewController)")
+    }
+}
+
+struct NoAlignLeftRule: Rule {
+    let name = "no_align_left"
+    let description = "No label should set its alignment to `left` directly. Use alignment `natural` instead"
+
+    func apply(on view: UIView) -> Bool {
+        if let label = view as? UILabel {
+            if label.textAlignment == .left {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+struct CellsAccessibilityLabelRule: Rule {
+    let name = "cells_accessibility_label"
+    let description = "Complex cells should have an accessibility label describing the content of them"
+
+    func apply(on view: UIView) -> Bool {
+        if let cell = view as? UITableViewCell,
+            cell.contentView.subviews.count > 3,
+            cell.isAccessibilityElement == false ||
+            cell.accessibilityLabel == nil {
+
+            return false
+        }
+
+        return true
+    }
+}
+
 extension UIViewController {
     @objc func runtimeLinter(_ animated: Bool) {
-        print("Subviews: ")
-        print(self.view.subviews.count)
+        self.runtimeLinter(animated) // this calls the original viewDidAppear implementation.
+
         if (self is UINavigationController || self is UITabBarController || self is UISplitViewController) {
             return
         }
         checkForSubviews(self.view)
     }
 
-    private func checkForSubviews(_ view: UIView) {
-        applyRules(to: view)
-
+    private func shouldInspectSubviews(_ view: UIView) -> Bool {
         if view is UIButton ||
             view is UISearchBar ||
             view is UITableViewCell {
-            return
+            return false
         }
 
-        for subView in view.subviews {
-            checkForSubviews(subView)
+        return true
+    }
+
+    private func checkForSubviews(_ view: UIView) {
+        applyRules(to: view)
+
+        if shouldInspectSubviews(view) {
+            for subView in view.subviews {
+                checkForSubviews(subView)
+            }
         }
     }
 
     private func applyRules(to view: UIView) {
-        applyRuleAccessibilityLabel(to: view)
-    }
-
-    private func applyRuleAccessibilityLabel(to view: UIView) {
-        if view is UILabel || view is UIButton {
-            if view.accessibilityLabel == nil {
-                let address = unsafeBitCast(view, to: Int.self)
-                DDLogWarn("⚠️♿️ Accessibility label missing in <\(type(of: view)): \(String(format: "%p", address))>, in \(self)")
+        rules.forEach {
+            if $0.apply(on: view) == false {
+                $0.printError(for: view, in: self)
             }
         }
     }
 
-    private func applyRuleNotAlignLeft(to view: UIView) {
-        if let label = view as? UILabel {
-            if label.textAlignment == .left {
-                DDLogWarn("⚠️ :Runtime Check: Forced align left found \(type(of: label)): '\(label.text ?? "")', in \(self)")
-            }
-        }
-    }
+    // MARK: - Swizzle Implementation
 
     private static let swizzleViewDidLoadImplementation: Void = {
         let aClass: AnyClass = UIViewController.self
