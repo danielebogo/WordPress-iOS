c2e5e148821eded535a5169d649ceb9cf6f4bd2a
Daniele Bogo <me@bogodaniele.com>
2018-04-15 11:23:53 +0100

    #8950
    - Fix localisation for Notice prompt

--- WordPress/Classes/ViewRelated/Reader/ReaderStreamViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderStreamViewController.swift
@@ -840,15 +840,16 @@ import WordPressFlux
             return
         }
         
-        let localizedTitle = NSLocalizedString("Following %@", comment: "Text of an OK button to dismiss a prompt.")
+        let localizedTitle = NSLocalizedString("Following %@", comment: "Localized title for the Notice to prompt")
         let title = String(format: localizedTitle, siteTitle)
-        let message = NSLocalizedString("Enable site notifications?", comment: "Text of an OK button to dismiss a prompt.")
+        let message = NSLocalizedString("Enable site notifications?", comment: "Notice message text")
+        let buttonTitle = NSLocalizedString("Enable", comment: "Notice button title text")
         
         let notice = Notice(title: title,
                             message: message,
                             feedbackType: .success,
                             notificationInfo: nil,
-                            actionTitle: "Enable") { [weak self] in
+                            actionTitle: buttonTitle) { [weak self] in
                                 self?.toggleSubscribingNotificationsFor(siteID: siteID, subscribe: true)
         }
         ActionDispatcher.dispatch(NoticeAction.post(notice))
